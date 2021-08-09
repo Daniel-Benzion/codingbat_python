a56e79c4ae25cb194cd74e0a77ae388266e64dd7
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-08-09 09:02:01 -0500

    end_other

---
+++ string-2/end_other.py
@@ -0,0 +1,7 @@
+def end_other(a, b):
+  a1 = a.lower()
+  b1 = b.lower()
+  if len(a) == len(b): return a1 == b1
+  if len(a) < len(b):
+    return b1[(len(a) * -1):] == a1
+  return a1[(len(b) * -1):] == b1
